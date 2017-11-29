46a99fb2a5d399ba7ed5ffa5e0b9d3e706636d24
eugene kim <eugkiqs@gmail.com>
2017-11-29 18:34:35 -0500

    pseudocodes only. emergencies came up!

---
+++ buttons.js
@@ -0,0 +1,29 @@
+// create initial empty array to hold inputted brand names 
+var nameArray = []
+
+// on click event associated with addBrand input field
+
+    $("#addBrand").on("click", function(event) {
+      event.preventDefault();
+
+// take inputted string and store into empty array 
+
+
+
+    });
+
+
+
+// pull nameArray item and convert back to string 
+
+// search string in GIPHY API for matching related gifs
+
+// search matching gif for parental guidance rating and append to gifDisplay div
+
+// append gifs below corresponding ratings in gifDisplay div
+
+// button click event to animate the gif once when clicked 
+
+
+
+
